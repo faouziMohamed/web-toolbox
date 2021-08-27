783735cdc1acee8e50ba76d017be1578655f7eaa
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-08-27 23:40:10 +0100

    Customize more Element !!

--- connected/js/headerMenu.js
+++ connected/js/headerMenu.js
@@ -1,39 +1,40 @@
 export const useHeaderMenu = () => {
   const input = document.querySelector('.burger-menu__checkbox');
   const burgerMenu = document.querySelector('.burger-menu');
-  if (!input || !burgerMenu) return;
-
   const contentRoot = document.querySelector('.main-content-root');
   const leftSide = document.querySelector('.left-side');
-  const toggle = () => {
-    leftSide.classList.toggle('left-side__closed');
-    contentRoot.classList.toggle(['prevent-scroll'], ['blur']);
-    burgerMenu.classList.toggle('left-side__oppened');
-  };
+  const leftSideParent = document.querySelector('.left-side-parent');
+  if (!input || !burgerMenu) return;
 
+  const toggle = () => toggleStates(leftSide, contentRoot, burgerMenu);
+  addEventHandlers(input, leftSideParent, toggle);
   setDefaultStates(leftSide, contentRoot, burgerMenu, input);
-  input.addEventListener('change', toggle);
-  const closeArea = document.querySelector('.left-side__clickToClose');
-  closeArea.addEventListener('click', () => {
-    toggle();
-    input.checked = false;
-  });
-
-  /**
-   *  const media = window.matchMedia('(min-width: 780px)');
-   *  const listenMedia = (x) => {
-   *    input.checked = false;
-   *   if (x.matches) {
-   *      ul.classList.remove('hidden');
-   *    } else {
-   *       ul.classList.add('hidden');
-   *     }
-   *   };
-   *   media.addEventListener('change', listenMedia);
-   *   listenMedia(media);
-   *
-   */
 };
+
+function addEventHandlers(input, leftSideParent, toggle) {
+  input.addEventListener('change', () => {
+    leftSideParent.classList.toggle('hidden');
+    toggle();
+  });
+  leftSideParent.addEventListener('click', closeLeftSide(input, toggle));
+}
+
+function closeLeftSide(input, toggle) {
+  return function (e) {
+    const { classList } = e.target;
+    if (!classList.contains('left-side-parent')) return;
+    this.classList.add('hidden');
+    input.checked = false;
+    toggle();
+  };
+}
+
+function toggleStates(leftSide, contentRoot, burgerMenu) {
+  leftSide.classList.toggle('left-side__closed');
+  contentRoot.classList.toggle(['prevent-scroll'], ['blur']);
+  burgerMenu.classList.toggle('left-side__oppened');
+}
+
 function setDefaultStates(leftSide, contentRoot, burgerMenu, input) {
   leftSide.classList.add('left-side__closed');
   leftSide.classList.remove('hidden');
